baab4d214ef775da2b51d36f390bbf0533528d84
M. Agung Isra Narwin <91040756+agungisra@users.noreply.github.com>
2022-04-20 13:30:37 +0700

    Add files via upload

---
+++ LatihanBiodata/biodata.php
@@ -0,0 +1,136 @@
+<?php 
+echo '<html>
+    <head>
+        <title>Homepage Pribadi</title>
+        <style>
+            body {
+                background-image: url("560870939635e7482489ef94fa580bb9.png");
+                background-repeat: no-repeat;
+                background-size:cover
+            }
+          </style>
+    </head>
+    <body>
+        <h1 align=center>CURRICULUM VITAE</h1>
+        <!--INFORMASI DATA DIRI-->
+            <hr>
+            <b>PERSONAL INFORMATION</b>
+            <hr>
+            <table style="width:50%">
+                <!--FOTO-->
+                <img src="fotosaya.png" align="right" width="104" height="142">
+            <tr>
+            <th></th>
+            <th></th>
+            </tr>
+            <tr>
+                <td>Nama Lengkap</td>
+                <td>: M. Agung Isra Narwin </td>
+            </tr>
+            <tr>
+                <td>Tempat Tanggal Lahir</td>
+                <td>: Solok, 27 September 2002 </td>   
+            </tr>
+                <td>Jenis Kelamin</td>
+                <td>: Laki-laki </td>
+            </tr>             
+                <td>Status</td>
+                <td>: Mahasiswa</td>
+            </tr>
+                <td>Agama</td>
+                <td>: Islam </td>
+            </tr>
+                <td>Kewarganegaraan</td>
+                <td>: Indonesia</td>
+            </tr>
+            </table>
+        <!--KONTAK-->   
+            <hr>
+            <b>KONTAK</b>
+            <hr>
+                <table style="width:185%">
+            <tr>
+            <th></th>
+            <th></th>
+            </tr>
+            <tr>
+                <tr>
+                    <td>NO HP</td>
+                    <td>081277116006</td>
+                </tr>
+                <tr>
+                    <td>Email</td>
+                    <td>agungisra27@gmail</td>
+                </tr>
+                    <td>Alamat</td>
+                    <td>Jl. Raya Tj. Balik, X Koto Diatas, Solok, Sumatera Barat</td>
+                <tr>
+            </table>
+        <!--RIWAYAT PENDIDIKAN-->   
+                <hr>
+                <b>PENDIDIKAN</b>
+                <hr>
+                    <table style="width:55%">
+                <tr>
+                <th></th>
+                <th></th>
+                </tr>
+                <tr>
+                    <td>2021-Sekarang</td>
+                    <td>Telkom University </td>
+                </tr>
+                <tr>
+                    <td>2018-2021</td>
+                    <td>SMA Negeri 1 Solok </td>   
+                </tr>
+                    <td>2015-2018</td>
+                    <td>SMP IT Iqra\' </td>
+                <tr>
+                    <td>2009-2015</td>
+                    <td>SDN 03 Tanjung Balik </td>
+                </tr>
+                </table>
+        <!-- RIWAYAT ORGANISASI-->
+                        <hr>
+                        <b>ORGANISASI</b><hr>
+                            <table style="width:178%">
+                        <tr>
+                        <th></th>
+                        <th></th>
+                        </tr>
+                        <tr>
+                            <td>2014</td>
+                            <td>Ketua Kedisiplinan Siswa</td>
+                        </tr>
+                        <tr>
+                            <td>2016</td>
+                            <td>Sekretaris 1 OSIS SMP It Iqra  </td>   
+                        </tr>
+                        <tr>
+                            <td>2018</td>
+                            <td>Anggota Ikatan Pelajar Tanjung Balit</td>   
+                        </tr>
+                        </table>
+        <!--KEMAMPUAN-->  
+                            <hr>
+                            <b>KEMAMPUAN</b><hr>
+                            <table style="width:55%">
+                            <tr>
+                            <th></th>
+                            <th></th>
+                            </tr>
+                            <tr>
+                                <td>Mengoperasikan Komputer</td>
+                                <td>Microsoft Office</td>
+                            </tr>
+                            <tr>
+                                <td>Desain Grafis</td>
+                                <td>CorelDRAW & Adobe Ilustrator</td>   
+                            </tr>
+                            <tr>
+                                <td>Editing</td>
+                                <td>Adobe Photoshop & Adobe Premiere Pro</td>   
+                            </tr>
+                            </table>
+    </body>
+</html>';
